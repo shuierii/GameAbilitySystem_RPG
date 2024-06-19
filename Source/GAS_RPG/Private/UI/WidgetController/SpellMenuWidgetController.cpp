f789f40e1390cef435db49ead19b91a19d2e221c
yxxjian <529903830@qq.com>
2024-06-19 15:10:15 +0800

    选中的技能取消选中

--- Source/GAS_RPG/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/GAS_RPG/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -99,6 +99,14 @@ void USpellMenuWidgetController::SpendPointButtonPressed()
 	}
 }
 
+void USpellMenuWidgetController::GlobeDeselect()
+{
+	SelectedAbility.Ability = FAuraGameplayTags::Get().Abilities_NONE;
+	SelectedAbility.Status = FAuraGameplayTags::Get().Abilities_Status_Locked;
+
+	SpellGlobeSelectedDelegate.Broadcast(false, false, FString(), FString());
+}
+
 void USpellMenuWidgetController::ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints, bool& bShouldEnableSpellPointsButton, bool& bShouldEnableEquipButton)
 {
 	const FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
