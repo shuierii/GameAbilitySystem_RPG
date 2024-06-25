89007ef6f7e67ad9d4ed71e641528e9f0aef4f89
yxxjian <529903830@qq.com>
2024-06-25 19:25:43 +0800

    魔法阵贴花Actor

---
+++ Source/GAS_RPG/Public/Actor/MagicCircle.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class GAS_RPG_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMagicCircle();
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+
+protected:
+	virtual void BeginPlay() override;
+};
