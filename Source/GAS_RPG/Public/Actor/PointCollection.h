7876d4af9b615996dc217d7d999b099fb333cd78
yxxjian <529903830@qq.com>
2024-06-26 17:19:00 +0800

    魔法阵生成水晶位置

---
+++ Source/GAS_RPG/Public/Actor/PointCollection.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PointCollection.generated.h"
+
+UCLASS()
+class GAS_RPG_API APointCollection : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APointCollection();
+
+	UFUNCTION(BlueprintPure)
+	TArray<USceneComponent*> GetGroundPoints(const FVector& GroundLocation, int32 NumPoints, float YawOverride = 0.f);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TArray<USceneComponent*> ImmutablePts;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_0;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_1;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_2;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_3;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_4;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_5;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_6;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_7;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_8;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_9;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Pt_10;
+};
