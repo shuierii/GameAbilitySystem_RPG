8b4f37bfaa9965fde91e157558114e588674bfd3
yxxjian <529903830@qq.com>
2024-07-04 17:29:40 +0800

    存档绑定等级信息

--- Source/GAS_RPG/Private/UI/ViewModel/MVVM_LoadScreen.cpp
+++ Source/GAS_RPG/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -43,6 +43,7 @@ void UMVVM_LoadScreen::NewSlotButtonPressed(int32 Slot, const FString& EnteredNa
 
 	LoadSlots[Slot]->SetMapName(AuraGameMode->DefaultMapName);
 	LoadSlots[Slot]->SetPlayerName(EnteredName);
+	LoadSlots[Slot]->SetPlayerLevel(1);
 	LoadSlots[Slot]->SlotStatus = Taken;
 	LoadSlots[Slot]->PlayerStartTag = AuraGameMode->DefaultPlayerStartTag;
 
@@ -120,7 +121,7 @@ void UMVVM_LoadScreen::LoadData()
 
 		LoadSlot.Value->SetMapName(SaveObject->MapName);
 		LoadSlot.Value->PlayerStartTag = SaveObject->PlayerStartTag;
-		// LoadSlot.Value->SetPlayerLevel(SaveObject->PlayerLevel);
+		LoadSlot.Value->SetPlayerLevel(SaveObject->PlayerLevel);
 	}
 }
 
